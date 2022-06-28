562ba3698f8d43bbaff71204c597dbc060b2475e
Nikesh Mistri <nikeshmistri@hotmail.co.uk>
2022-06-28 10:55:27 +0100

    update page

--- backend/capstone-fullstack-app/src/main/java/com/example/capstonefullstackapp/Service/PageService.java
+++ backend/capstone-fullstack-app/src/main/java/com/example/capstonefullstackapp/Service/PageService.java
@@ -1,11 +1,14 @@
 package com.example.capstonefullstackapp.Service;
 
+import com.example.capstonefullstackapp.Model.Category;
 import com.example.capstonefullstackapp.Model.Page;
 import com.example.capstonefullstackapp.Repository.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PageService {
@@ -32,5 +35,14 @@ public class PageService {
 
     public void savePage(Page page) {
         pageRepository.save(page);
+    }
+
+
+    public Page updatePage(Long id, Page updatedPageInfo) {
+        Page oldPage = pageRepository.findById(id).get();
+        oldPage.setTitle(updatedPageInfo.getTitle());
+        oldPage.setContent(updatedPageInfo.getContent());
+        oldPage.setBackground(updatedPageInfo.getBackground());
+        return pageRepository.save(oldPage);
     }
 }
