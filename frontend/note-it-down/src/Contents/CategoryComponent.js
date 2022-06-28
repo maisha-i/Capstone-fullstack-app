d8315b218c9cc1e149978de1f9d9d9fa55153ce6
James Stillman <jamesstillman99@gmail.com>
2022-06-28 15:08:57 +0100

    Finished basic category component

--- frontend/note-it-down/src/Contents/CategoryComponent.js
+++ frontend/note-it-down/src/Contents/CategoryComponent.js
@@ -1,25 +1,24 @@
-
+import './CategoryComponent.css'
 
 const CategoryComponent = ({category}) => {
 
     const {title, pages} = category;
 
     return(
-        <>
+        <section>
             <h1>{title}</h1>
-            <ul>
+            <ul className="contents-page--page-list">
             {pages.map(page => { return(
                 <li>{page.title}</li>
             )
 
             })}
-
             </ul>
         
         
         
         
-        </>
+        </section>
     )
 }
 
