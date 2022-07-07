8fdfbf4319848e64c733d76e62e326b2a25ff414
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-07-07 12:28:59 +0100

    removed commented out sections of code no longer needed

--- frontend/note-it-down/src/Login/UserLogin.js
+++ frontend/note-it-down/src/Login/UserLogin.js
@@ -1,41 +1,11 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { RiLockPasswordFill, RiUser3Fill} from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
 
-
-
-
 const UserLogin = ({handleChange, loginFunction}) => {
-
-        // const [users, setUsers] = useState("");
-
-    // use state - tracks the state of the browser i.e. chanegs in the browser for every key stroke. 
-    // const[email, setEmail] = useState("");
-    // const[password, setPassword] = useState("");
-
-    // const handleEmail = event => setEmail(event.target.value);
-    // const handlePassword = event => setPassword(event.target.value);
-    
-
-    // const handleSubmit = event =>{
-    //     event.preventDefault();
-
-    //     const user = {
-    //         "email":email,
-    //         "password":password
-    //     }  
-
-    //     axios.post("http://127.0.0.1:8080/user", users)
-    //   .then(res => {
-    //       console.log(res);
-    //   }).catch((err) => console.log(err));
-    // }
 
     return(
        <>   <div className="login-box">
                 <div className="login-form"> 
-                    {/* <p>Log In </p>    */}
 
                     <div className="email">
                         {/* value = username set out inline 4  - bind the username and password to the text boxes 
@@ -43,8 +13,6 @@ const UserLogin = ({handleChange, loginFunction}) => {
                         
                         <MdEmail id="icon-l-email"/><input 
                         type="email" placeholder="Email" id="email" name="email"
-                        // value={email} 
-                        // onChange={(event) => setEmail(event.target.value)}
                         onChange={handleChange}
                         /> 
                     </div>
@@ -53,8 +21,6 @@ const UserLogin = ({handleChange, loginFunction}) => {
                     <RiLockPasswordFill id="icon-l-password"/><input 
                         type="password" placeholder="Password"
                         id="password" name="password"
-                        // value={password}
-                        // onChange={(event) => setPassword(event.target.value)}
                         onChange={handleChange} 
                         /> 
                     </div> 
